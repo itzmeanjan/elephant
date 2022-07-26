e23cd5c017906b5a1568ac3e27e19d3c11f1f827
Anjan Roy <hello@itzmeanjan.in>
2022-07-26 11:12:17 +0400

    extract i-th data block, used when authenticating associated data
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/aead.hpp
+++ include/aead.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "spongent.hpp"
+#include <algorithm>
 #include <bit>
 
 // Elephant Authenticated Encryption with Associated Data
@@ -75,6 +76,60 @@ next_mask(const uint8_t* const __restrict key,
       fmask[i] ^= tmp[i];
     }
   }
+}
+
+// When authenticating associated data, this routine extracts out requested i-th
+// block from {pre, post}-padded associated data.
+//
+// Note,
+//
+// - associated data is prepended with 12 -bytes nonce
+// - associated data is appended with byte value 1, which might incur zero
+// padding
+//
+// See step 5 of algorithm 1 & 2, in Elephant specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/elephant-spec-final.pdf
+//
+// Template parameter `slen` expects value {160, 176}, which is the block length
+// of underlying primitive (permutation). When using this function for
+//
+// - Dumbo, do slen = 160
+// - Jumbo, do slen = 176
+//
+// This implementation collects some inspiration from
+// https://github.com/TimBeyne/Elephant/blob/b1a6883/crypto_aead/elephant160v2/ref/encrypt.c#L37-L68
+template<const size_t slen>
+static void
+get_ith_data_block(
+  const uint8_t* const __restrict data,  // N -bytes associated data
+  const size_t dlen,                     // len(data) = N | >= 0
+  const uint8_t* const __restrict nonce, // 12 -bytes public message nonce
+  const size_t i,               // index ( zero based ) of block to extract
+  uint8_t* const __restrict blk // extracted block to be placed here
+  ) requires(spongent::check_state_bit_len(slen))
+{
+  constexpr size_t blk_len = slen >> 3;
+  constexpr uint8_t pad = 0x01;
+
+  size_t off = 0;
+
+  std::memcpy(blk, nonce, 12 * (i == 0));
+  off += 12 * (i == 0);
+
+  const size_t doff = i * blk_len - 12 * (i > 0);
+  const size_t tot_to_read = blk_len - off;
+  const size_t data_to_read = std::min(tot_to_read, dlen - doff);
+
+  std::memcpy(blk + off, data + doff, data_to_read);
+  off += data_to_read;
+
+  const size_t rm_to_read = blk_len - off;
+  const size_t rd_bytes = std::min(rm_to_read, 1);
+
+  std::memcpy(blk + off, &pad, rd_bytes);
+  off += rd_bytes;
+
+  std::memset(blk + off, 0, blk_len - off);
 }
 
 }
