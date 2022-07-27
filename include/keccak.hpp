ec5273c619b3617df447a5fcfe1fbb9612d37eaa
Anjan Roy <hello@itzmeanjan.in>
2022-07-27 16:19:58 +0400

    keccak-f[200] step mapping function `ι`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/keccak.hpp
+++ include/keccak.hpp
@@ -21,6 +21,67 @@ constexpr size_t ROT[]{ 0 & 7,   1 & 7,   190 & 7, 28 & 7,  91 & 7,
                         105 & 7, 45 & 7,  15 & 7,  21 & 7,  136 & 7,
                         210 & 7, 66 & 7,  253 & 7, 120 & 7, 78 & 7 };
 
+// Computes single bit of Keccak-f[200] round constant ( at compile-time ),
+// using binary LFSR, defined by primitive polynomial x^8 + x^6 + x^5 + x^4 + 1
+//
+// See algorithm 5 in section 3.2.5 of http://dx.doi.org/10.6028/NIST.FIPS.202
+consteval bool
+rc(const size_t t)
+{
+  // step 1 of algorithm 5
+  if (t % 255 == 0) {
+    return 1;
+  }
+
+  // step 2 of algorithm 5
+  //
+  // note, step 3.a of algorithm 5 is also being
+  // executed in this statement ( for first iteration, with i = 1 ) !
+  uint16_t r = 0b10000000;
+
+  // step 3 of algorithm 5
+  for (size_t i = 1; i <= t % 255; i++) {
+    const uint16_t b0 = r & 1;
+
+    r = (r & 0b011111111) ^ ((((r >> 8) & 1) ^ b0) << 8);
+    r = (r & 0b111101111) ^ ((((r >> 4) & 1) ^ b0) << 4);
+    r = (r & 0b111110111) ^ ((((r >> 3) & 1) ^ b0) << 3);
+    r = (r & 0b111111011) ^ ((((r >> 2) & 1) ^ b0) << 2);
+
+    // step 3.f of algorithm 5
+    //
+    // note, this statement also executes step 3.a for upcoming
+    // iterations ( i.e. when i > 1 )
+    r >>= 1;
+  }
+
+  return static_cast<bool>((r >> 7) & 1);
+}
+
+// Computes 8 -bit round constant ( at compile-time ), which is XOR-ed into very
+// first lane of Keccak-f[200] permutation state
+consteval uint8_t
+compute_rc(const size_t r_idx)
+{
+  uint8_t tmp = 0;
+
+  for (size_t j = 0; j < 4; j++) {
+    const size_t boff = (1 << j) - 1;
+    tmp |= static_cast<uint8_t>(rc(j + 7 * r_idx)) << boff;
+  }
+
+  return tmp;
+}
+
+// Round constants to be XORed with lane (0, 0) of keccak-f[200] permutation
+// state, see section 3.2.5 of https://dx.doi.org/10.s6028/NIST.FIPS.202
+constexpr uint8_t RC[ROUNDS]{ compute_rc(0),  compute_rc(1),  compute_rc(2),
+                              compute_rc(3),  compute_rc(4),  compute_rc(5),
+                              compute_rc(6),  compute_rc(7),  compute_rc(7),
+                              compute_rc(9),  compute_rc(10), compute_rc(11),
+                              compute_rc(12), compute_rc(13), compute_rc(14),
+                              compute_rc(15), compute_rc(16), compute_rc(17) };
+
 // Keccak-f[200] step mapping function, see section 3.2.1 of SHA3 specification
 // https://dx.doi.org/10.6028/NIST.FIPS.202
 inline static void
@@ -98,6 +159,14 @@ chi(const uint8_t* const __restrict istate, // input permutation state
       ostate[ix5 + j] = istate[ix5 + j] ^ rhs;
     }
   }
+}
+
+// Keccak-f[200] step mapping function, see section 3.2.5 of SHA3 specification
+//  https://dx.doi.org/10.6028/NIST.FIPS.202
+inline static void
+iota(uint8_t* const state, const size_t r_idx)
+{
+  state[0] ^= RC[r_idx];
 }
 
 }
